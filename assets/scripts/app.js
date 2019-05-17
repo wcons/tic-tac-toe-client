74b19d9c56ff024fac29a7a4012dac66213cc482
wcons <wconstantino@gmail.com>
2019-05-16 20:57:49 -0400

    Add Event Handler for gameboard
    Add event handler for gameboard button clicks

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const authEvents = require('./auth/events.js')
+const gameEvents = require('./game/events.js')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
@@ -12,4 +13,6 @@ $(() => {
   $('#sign-in').on('submit', authEvents.onSignIn)
   $('#change-pw').on('submit', authEvents.onChangePassword)
   $('#sign-out').on('submit', authEvents.onSignOut)
+
+  $('div.gameboard button').on('click', gameEvents.onButtonClick)
 })
---
+++ assets/scripts/game/events.js
@@ -0,0 +1,7 @@
+'use strict'
+const onButtonClick = event => {
+  console.log(event.target.id)
+}
+module.exports = {
+  onButtonClick
+}
