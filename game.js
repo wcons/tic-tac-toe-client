9283da9a7405eba3bf6898413f124d31494a37c8
wcons <wconstantino@gmail.com>
2019-05-19 10:53:05 -0400

    Add Game Logic
    Add draft game logic

---
+++ game.js
@@ -0,0 +1,54 @@
+const gameBoard = ['', '', '', '', '', '', '', '', '']
+
+let move = 0
+
+const winningMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+const makeMove = function (id) {
+
+  let token = ''
+  if (move % 2 === 0) {
+    token = 'x'
+  } else {
+    token = 'o'
+  }
+  gameBoard[id] = token
+  for (let i = 0; i < winningMoves.length; i++) {
+    // i = 3
+    let threeIndices = winningMoves[i] // -> [0,3,6]
+    //
+    // [
+    //   0 => 0,
+    //   1 => 3,
+    //   2 => 6
+    // ]
+    //threeIndices[0]  === 0
+    let first = gameBoard[threeIndices[0]] // 'o'
+    let second = gameBoard[threeIndices[1]]//'o'
+    let third = gameBoard[threeIndices[2]]//'o'
+
+    if (first === second && second === third && first !== '') {
+      console.log('Winner!')
+      return true
+    }
+  }
+  move++
+}
+
+module.export = {
+  makeMove
+}
+
+
+
+console.log(gameBoard)
+makeMove(0)
+console.log(gameBoard)
+makeMove(5)
+console.log(gameBoard)
+makeMove(2)
+console.log(gameBoard)
+makeMove(6)
+console.log(gameBoard)
+makeMove(1)
+console.log(gameBoard)
